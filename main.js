a25c405396c33a5017f9bed193b3e611f49f78db
Noah <nbelman1@gmail.com>
2025-07-08 22:03:48 -0400

    Complete tasks 1, 2, 3

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-code
+// Task 1: Initialize the Array
+let greetings = Array(7);
+greetings.fill("Hello");
+console.log(greetings);
+
+// Task 2: Update the Array
+greetings.fill("Hi", 2, 4);
+console.log(greetings);
+
+// Task 3: Populate the Array with a for loop
+let numberList = Array(5);
+for (i = 0; i < numberList.length; i++) {
+    numberList[i] = i * 10;
+}
+console.log(numberList);
